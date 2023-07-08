9aecaaed850a0c489291e1ca09d40440b84b83ee
Bruno <bfaa1999@gmail.com>
2023-07-08 17:02:33 -0300

    fix: atualizando transações ao criar uma ordem de pagamento

--- packages/api/src/controllers/company/companyPaymentOrder/GeneratePaymentOrderUseCase.ts
+++ packages/api/src/controllers/company/companyPaymentOrder/GeneratePaymentOrderUseCase.ts
@@ -107,12 +107,13 @@ class GeneratePaymentOrderUseCase {
     });
 
     // Atualizando as transações
-    transactionsLocalized.map(async (item) => {
-      await getRepository(Transactions).update(item.transaction_id, {
-        paymentOrder,
-        transactionStatus: processStatus,
-      });
-    });
+    await prisma.transaction.updateMany({
+      where: { id: { in: transactionIDs } },
+      data: {
+        transactionStatusId: processStatus.id,
+        paymentOrderId: paymentOrder.id
+      }
+    })
 
     const users = await prisma.takebackUser.findMany({
       where: { userTypeId: 2, isActive: true },
