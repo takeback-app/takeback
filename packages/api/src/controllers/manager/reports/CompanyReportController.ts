3dae12f192450049a21a31a7a25fc3763bbfd5f9
Fabiano Santos <50252807+fabianosantos23@users.noreply.github.com>
2023-08-31 10:33:25 -0300

    chore: remove função desnecessária

--- packages/api/src/controllers/manager/reports/CompanyReportController.ts
+++ packages/api/src/controllers/manager/reports/CompanyReportController.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express'
-import { DateTime } from 'luxon'
 import { InternalError } from '../../../config/GenerateErros'
-import { prisma } from '../../../prisma'
 import { filterNumber } from '../../../utils'
 import { ManagerCompanyReportRequest } from '../../../requests/reports/manager/ManagerCompanyReportRequest'
 import { CompanyReport } from '../../../reports/manager/CompanyReport'
@@ -169,83 +167,5 @@ export class CompanyReportController {
     })
 
     return response.status(200).json(sumTotalizer)
-  }
-
-  async totalizer(request: Request, response: Response) {
-    const {
-      dateEnd,
-      dateStart,
-      cityId,
-      stateId,
-      companyStatusId,
-      transactionStatusId,
-    } = request.query as Record<string, string>
-
-    const startDate = dateStart
-      ? DateTime.fromISO(dateStart).startOf('day').toJSDate()
-      : undefined
-    const endDate = dateEnd
-      ? DateTime.fromISO(dateEnd).endOf('day').toJSDate()
-      : undefined
-
-    let companyAddress
-
-    if (cityId && stateId) {
-      companyAddress = {
-        AND: [
-          { cityId: filterNumber(cityId) },
-          {
-            city: { stateId: filterNumber(stateId) },
-          },
-        ],
-      }
-    }
-
-    if (!cityId && stateId) {
-      companyAddress = {
-        city: { stateId: filterNumber(stateId) },
-      }
-    }
-
-    const companiesCount = await prisma.company.count({
-      where: {
-        companyAddress,
-        statusId: filterNumber(companyStatusId),
-      },
-    })
-
-    const positiveBalances = await prisma.company.aggregate({
-      where: {
-        companyAddress,
-        statusId: filterNumber(companyStatusId),
-      },
-      _sum: {
-        positiveBalance: true,
-      },
-    })
-
-    const cashbacks = await prisma.transaction.aggregate({
-      where: {
-        createdAt: { gte: startDate, lte: endDate },
-        transactionStatusId: filterNumber(transactionStatusId),
-        company: {
-          companyAddress,
-          statusId: filterNumber(companyStatusId),
-        },
-      },
-      _sum: {
-        totalAmount: true,
-        takebackFeeAmount: true,
-        cashbackAmount: true,
-      },
-    })
-
-    return response.status(200).json({
-      companiesCount,
-      totalAmount: cashbacks._sum.totalAmount,
-      totalCashbackAmount: cashbacks._sum.cashbackAmount,
-      totalTakebackFeeAmount: cashbacks._sum.takebackFeeAmount,
-      totalPositiveBalances: positiveBalances._sum.positiveBalance,
-    })
   }
 }
