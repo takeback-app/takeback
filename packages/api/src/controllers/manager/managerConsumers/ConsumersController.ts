902e7e0bed6f1067b41f61cc91fa693d1108a1f7
Fabiano Santos <50252807+fabianosantos23@users.noreply.github.com>
2023-08-31 17:24:07 -0300

    chore: prettier formata documento

--- packages/api/src/controllers/manager/managerConsumers/ConsumersController.ts
+++ packages/api/src/controllers/manager/managerConsumers/ConsumersController.ts
@@ -1,71 +1,71 @@
-import { Request, Response } from "express";
-import { FindConsumersDataUseCase } from "./FindConsumersDataUseCase";
-import { ListCitiesUseCase } from "./ListCitiesUseCase";
-import { FindConsumersUseCase } from "./FindConsumersUseCase";
-import { UpdateStatusConsumerUseCase } from "./UpdateStatusConsumerUseCase";
-import { ForgotCostumerPasswordUseCase } from "./ForgotConsumerPasswortUseCase";
+import { Request, Response } from 'express'
+import { FindConsumersDataUseCase } from './FindConsumersDataUseCase'
+import { ListCitiesUseCase } from './ListCitiesUseCase'
+import { FindConsumersUseCase } from './FindConsumersUseCase'
+import { UpdateStatusConsumerUseCase } from './UpdateStatusConsumerUseCase'
+import { ForgotCostumerPasswordUseCase } from './ForgotConsumerPasswortUseCase'
 
 class ConsumersController {
   async findConsumers(request: Request, response: Response) {
-    const filters = request.query;
+    const filters = request.query
 
-    const find = new FindConsumersUseCase();
+    const find = new FindConsumersUseCase()
 
-    const result = await find.execute({ filters });
+    const result = await find.execute({ filters })
 
-    return response.status(200).json(result);
+    return response.status(200).json(result)
   }
 
   async findOneConsumer(request: Request, response: Response) {
-    const consumerId = request.params.id;
+    const consumerId = request.params.id
 
-    const find = new FindConsumersDataUseCase();
+    const find = new FindConsumersDataUseCase()
 
     const consumerData = await find.execute({
       consumerId,
-    });
+    })
 
-    return response.status(200).json({ consumerData });
+    return response.status(200).json({ consumerData })
   }
 
   async listCities(request: Request, response: Response) {
-    const list = new ListCitiesUseCase();
+    const list = new ListCitiesUseCase()
 
-    const result = await list.execute();
+    const result = await list.execute()
 
-    return response.status(200).json(result);
+    return response.status(200).json(result)
   }
 
   async updateConsumerStatus(request: Request, response: Response) {
-    const consumerId = request.params.id;
-    const { deactivedAccount } = request.body;
+    const consumerId = request.params.id
+    const { deactivedAccount } = request.body
 
-    const update = new UpdateStatusConsumerUseCase();
-    const find = new FindConsumersDataUseCase();
+    const update = new UpdateStatusConsumerUseCase()
+    const find = new FindConsumersDataUseCase()
 
     const message = await update.execute({
       consumerId,
       deactivedAccount,
-    });
+    })
 
     const consumerData = await find.execute({
       consumerId,
-    });
+    })
 
-    return response.status(200).json({ message, consumerData });
+    return response.status(200).json({ message, consumerData })
   }
 
   async forgotCostumerPassword(request: Request, response: Response) {
-    const { id } = request.params;
+    const { id } = request.params
 
-    const { email } = request.body;
+    const { email } = request.body
 
-    const forgot = new ForgotCostumerPasswordUseCase();
+    const forgot = new ForgotCostumerPasswordUseCase()
 
-    const message = await forgot.execute({ id, email });
+    const message = await forgot.execute({ id, email })
 
-    return response.status(200).json({ message });
+    return response.status(200).json({ message })
   }
 }
 
-export { ConsumersController };
+export { ConsumersController }
