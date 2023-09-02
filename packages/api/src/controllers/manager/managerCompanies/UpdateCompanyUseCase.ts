a23986be73022a7dfb9c64db860edef63e13cc7d
Fabiano Santos <50252807+fabianosantos23@users.noreply.github.com>
2023-09-02 14:25:15 -0300

    chore: prettier formata o documento

--- packages/api/src/controllers/manager/managerCompanies/UpdateCompanyUseCase.ts
+++ packages/api/src/controllers/manager/managerCompanies/UpdateCompanyUseCase.ts
@@ -1,55 +1,55 @@
-import { getRepository } from "typeorm";
-import { InternalError } from "../../../config/GenerateErros";
-import { City } from "../../../database/models/City";
-import { Companies } from "../../../database/models/Company";
-import { CompaniesAddress } from "../../../database/models/CompanyAddress";
-import { Industries } from "../../../database/models/Industry";
+import { getRepository } from 'typeorm'
+import { InternalError } from '../../../config/GenerateErros'
+import { City } from '../../../database/models/City'
+import { Companies } from '../../../database/models/Company'
+import { CompaniesAddress } from '../../../database/models/CompanyAddress'
+import { Industries } from '../../../database/models/Industry'
 
 interface UpdateProps {
-  fantasyName: string;
-  registeredNumber: string;
-  corporateName: string;
-  phone: string;
-  email: string;
-  industryId: string;
-  id: string;
-  companyId: string;
-  cityId: string;
-  street: string;
-  number: string;
-  district: string;
   longitude: string;
   latitude: string;
-  contactPhone: string;
+  fantasyName: string
+  registeredNumber: string
+  corporateName: string
+  phone: string
+  email: string
+  industryId: string
+  id: string
+  companyId: string
+  cityId: string
+  street: string
+  number: string
+  district: string
+  contactPhone: string
 }
 
 class UpdateCompanyUseCase {
   async execute(props: UpdateProps) {
     if (!props.email || !props.corporateName || !props.fantasyName) {
-      throw new InternalError("Dados incompletos", 400);
+      throw new InternalError('Dados incompletos', 400)
     }
 
     const company = await getRepository(Companies).findOne({
       where: { id: props.companyId },
-      relations: ["address"],
-    });
+      relations: ['address'],
+    })
 
     if (!company) {
-      throw new InternalError("Empresa não encontrada", 400);
+      throw new InternalError('Empresa não encontrada', 400)
     }
 
     const city = await getRepository(City).findOne({
       where: { id: props.cityId },
-    });
+    })
 
     if (!city) {
-      throw new InternalError("Cidade não encontrada", 400);
+      throw new InternalError('Cidade não encontrada', 400)
     }
 
-    const industry = await getRepository(Industries).findOne(props.industryId);
+    const industry = await getRepository(Industries).findOne(props.industryId)
 
     if (!industry) {
-      throw new InternalError("Ramo de Atividade inexistente", 401);
+      throw new InternalError('Ramo de Atividade inexistente', 401)
     }
 
     const updateCompanyAddress = await getRepository(CompaniesAddress).update(
@@ -61,11 +61,11 @@ class UpdateCompanyUseCase {
         street: props.street,
         longitude: props.longitude,
         latitude: props.latitude,
-      }
-    );
+      },
+    )
 
     if (updateCompanyAddress.affected === 0) {
-      throw new InternalError("Erro ao atualizar endereço da empresa", 500);
+      throw new InternalError('Erro ao atualizar endereço da empresa', 500)
     }
 
     const updateCompany = await getRepository(Companies).update(
@@ -73,20 +73,20 @@ class UpdateCompanyUseCase {
       {
         email: props.email,
         industry,
-        registeredNumber: props.registeredNumber.replace(/[^\d]/g, ""),
+        registeredNumber: props.registeredNumber.replace(/[^\d]/g, ''),
         fantasyName: props.fantasyName,
         corporateName: props.corporateName,
-        phone: props.phone.replace(/[^\d]/g, ""),
-        contactPhone: props.contactPhone.replace(/[^\d]/g, ""),
-      }
-    );
+        phone: props.phone.replace(/[^\d]/g, ''),
+        contactPhone: props.contactPhone.replace(/[^\d]/g, ''),
+      },
+    )
 
     if (updateCompany.affected === 0) {
-      throw new InternalError("Erro ao atualizar dados da empresa", 500);
+      throw new InternalError('Erro ao atualizar dados da empresa', 500)
     }
 
-    return "Empresa atualizada";
+    return 'Empresa atualizada'
   }
 }
 
-export { UpdateCompanyUseCase };
+export { UpdateCompanyUseCase }
--- packages/api/src/controllers/representative/CompanyController.ts
+++ packages/api/src/controllers/representative/CompanyController.ts
@@ -1,31 +1,31 @@
-import { Request, Response } from "express";
-import { Prisma } from "@prisma/client";
+import { Request, Response } from 'express'
+import { Prisma } from '@prisma/client'
 
-import { prisma } from "../../prisma";
+import { prisma } from '../../prisma'
 
-import { CreateCompanyUseCase } from "../../useCases/representative/CreateCompanyUserCase";
+import { CreateCompanyUseCase } from '../../useCases/representative/CreateCompanyUserCase'
 
-import { RegisterCompanyDefaultPaymentMethodsUseCase } from "../manager/managerCompanies/RegisterCompanyDefaultPaymentMethodsUseCase";
-import { AllowCompanyFirstAccessUseCase } from "../manager/managerCompanies/AllowCompanyFirstAccessUseCase";
-import { ForgotPasswordToRootUserUseCase } from "../manager/managerCompanies/ForgotPasswordToRootUserUseCase";
-import { FindCompanyUsersUseCase } from "../manager/managerCompanies/FindCompanyUsersUseCase";
-import { UpdateCompanyUseCase } from "../manager/managerCompanies/UpdateCompanyUseCase";
-import { GetRepresentative } from "../../useCases/representative/GetRepresentativeUseCase";
-import { maskCNPJ, maskPhone } from "../../utils/Masks";
+import { RegisterCompanyDefaultPaymentMethodsUseCase } from '../manager/managerCompanies/RegisterCompanyDefaultPaymentMethodsUseCase'
+import { AllowCompanyFirstAccessUseCase } from '../manager/managerCompanies/AllowCompanyFirstAccessUseCase'
+import { ForgotPasswordToRootUserUseCase } from '../manager/managerCompanies/ForgotPasswordToRootUserUseCase'
+import { FindCompanyUsersUseCase } from '../manager/managerCompanies/FindCompanyUsersUseCase'
+import { UpdateCompanyUseCase } from '../manager/managerCompanies/UpdateCompanyUseCase'
+import { GetRepresentative } from '../../useCases/representative/GetRepresentativeUseCase'
+import { maskCNPJ, maskPhone } from '../../utils/Masks'
 
-const PER_PAGE = 25;
+const PER_PAGE = 25
 
 export class CompanyController {
   async index(request: Request, response: Response) {
-    const { id } = request["tokenPayload"];
+    const { id } = request['tokenPayload']
 
-    const pageQuery = request.query.page;
+    const pageQuery = request.query.page
     const { company, industryId, statusId, cityId, monthlyPayment } =
-      request.query as Record<string, string>;
+      request.query as Record<string, string>
 
-    const page = Number(pageQuery) || 1;
+    const page = Number(pageQuery) || 1
 
-    const { whereCondominiumFilter } = await GetRepresentative.handle(id);
+    const { whereCondominiumFilter } = await GetRepresentative.handle(id)
 
     const where: Prisma.CompanyWhereInput = {
       ...whereCondominiumFilter,
@@ -50,7 +50,7 @@ export class CompanyController {
           ? monthlyPayment == "true"
           : undefined,
       },
-    };
+    }
 
     const companies = await prisma.company.findMany({
       where,
@@ -60,33 +60,33 @@ export class CompanyController {
       },
       take: PER_PAGE,
       skip: (page - 1) * PER_PAGE,
-    });
+    })
 
-    const count = await prisma.company.count({ where });
+    const count = await prisma.company.count({ where })
 
     return response.json({
       data: companies,
       meta: { lastPage: Math.ceil(count / PER_PAGE) },
-    });
+    })
   }
 
   async rootUser(request: Request, response: Response) {
-    const { id } = request.params;
+    const { id } = request.params
 
     const user = await prisma.companyUser.findFirstOrThrow({
       where: { companyId: id, isRootUser: true },
-    });
+    })
 
     return response.json({
       email: user.email,
       name: user.name,
       isDifferentEmail: false,
       isDifferentName: false,
-    });
+    })
   }
 
   async show(request: Request, response: Response) {
-    const { id } = request.params;
+    const { id } = request.params
 
     const company = await prisma.company.findUnique({
       where: { id },
@@ -104,24 +104,24 @@ export class CompanyController {
         companyStatus: { select: { description: true } },
         paymentPlan: { select: { description: true, value: true } },
       },
-    });
+    })
 
     const consultant = await prisma.representativeUserCompany.findFirst({
       where: { companyId: id },
-    });
+    })
 
-    company.registeredNumber = maskCNPJ(company.registeredNumber);
-    company.phone = maskPhone(company.phone);
-    company.contactPhone = maskPhone(company.contactPhone);
+    company.registeredNumber = maskCNPJ(company.registeredNumber)
+    company.phone = maskPhone(company.phone)
+    company.contactPhone = maskPhone(company.contactPhone)
 
     return response.json({
       ...company,
-      consultantId: consultant?.representativeUserId ?? "",
-    });
+      consultantId: consultant?.representativeUserId ?? '',
+    })
   }
 
   async store(request: Request, response: Response) {
-    const { representativeId } = request["tokenPayload"];
+    const { representativeId } = request['tokenPayload']
 
     const {
       industryId,
@@ -134,9 +134,9 @@ export class CompanyController {
       consultantId,
       paymentPlanId,
       companyUserData,
-    } = request.body;
+    } = request.body
 
-    const useCase = new CreateCompanyUseCase();
+    const useCase = new CreateCompanyUseCase()
 
     const company = await useCase.handle({
       industryId: Number(industryId),
@@ -148,36 +148,36 @@ export class CompanyController {
       zipCode,
       paymentPlanId,
       representativeId,
-    });
+    })
 
     if (consultantId) {
       await prisma.representativeUserCompany.create({
         data: { companyId: company.id, representativeUserId: consultantId },
-      });
+      })
     }
 
     const registerDefaultMethods =
-      new RegisterCompanyDefaultPaymentMethodsUseCase();
-    const allowCompanyAccess = new AllowCompanyFirstAccessUseCase();
+      new RegisterCompanyDefaultPaymentMethodsUseCase()
+    const allowCompanyAccess = new AllowCompanyFirstAccessUseCase()
 
     await allowCompanyAccess.execute({
       companyId: company.id,
       ...companyUserData,
-    });
+    })
 
-    await registerDefaultMethods.execute(company.id);
+    await registerDefaultMethods.execute(company.id)
 
-    return response.status(201).json(company);
+    return response.status(201).json(company)
   }
 
   async update(request: Request, response: Response) {
-    const props = request.body;
-    const { id } = request["tokenPayload"];
-    const companyId = request.params.id;
+    const props = request.body
+    const { id } = request['tokenPayload']
+    const companyId = request.params.id
 
-    const { cityId, district, number, street } = props.companyAddress;
+    const { cityId, district, number, street } = props.companyAddress
 
-    const update = new UpdateCompanyUseCase();
+    const update = new UpdateCompanyUseCase()
 
     const message = await update.execute({
       id,
@@ -193,38 +193,38 @@ export class CompanyController {
       number: number,
       street: street,
       contactPhone: props.contactPhone,
-    });
+    })
 
-    return response.status(200).json({ message });
+    return response.status(200).json({ message })
   }
 
   async updateConsultant(request: Request, response: Response) {
-    const companyId = request.params.id;
-    const { consultantId } = request.body;
+    const companyId = request.params.id
+    const { consultantId } = request.body
 
     await prisma.representativeUserCompany.deleteMany({
       where: { companyId },
-    });
+    })
 
     if (consultantId) {
       await prisma.representativeUserCompany.create({
         data: { companyId, representativeUserId: consultantId },
-      });
+      })
     }
 
-    return response.status(200).json({ message: "Consultor atualizado" });
+    return response.status(200).json({ message: 'Consultor atualizado' })
   }
 
   async forgotPasswordToRootUser(request: Request, response: Response) {
-    const companyId = request.params.id;
-    const { userName, email } = request.body;
+    const companyId = request.params.id
+    const { userName, email } = request.body
 
-    const forgot = new ForgotPasswordToRootUserUseCase();
-    const findUser = new FindCompanyUsersUseCase();
+    const forgot = new ForgotPasswordToRootUserUseCase()
+    const findUser = new FindCompanyUsersUseCase()
 
-    const message = await forgot.execute({ companyId, email, userName });
-    const users = await findUser.execute({ companyId });
+    const message = await forgot.execute({ companyId, email, userName })
+    const users = await findUser.execute({ companyId })
 
-    return response.status(200).json({ message, users });
+    return response.status(200).json({ message, users })
   }
 }
