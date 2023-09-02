105d3b3f59c04ca6534153b30774ce2212d511b3
Bruno <bfaa1999@gmail.com>
2023-09-02 12:19:15 -0300

    fix: raffle

--- packages/api/src/controllers/company/raffle/RaffleController.ts
+++ packages/api/src/controllers/company/raffle/RaffleController.ts
@@ -1,25 +1,25 @@
-import { Request, Response } from "express";
-import { prisma } from "../../../prisma";
-import { RaffleStatusEnum } from "../../../enum/RaffleStatusEnum";
-import { InternalError } from "../../../config/GenerateErros";
-import { CreateRaffleRequest } from "../../../requests/CreateRaffleRequest";
-import { UpdateRaffleRequest } from "../../../requests/UpdateRaffleRequest";
-import { ValidateNumberOfMonthlyRafflesUseCase } from "../../../useCases/raffle/ValidateNumberOfMonthlyRafflesUseCase";
-import { DateTime } from "luxon";
-import { UpdateOrCreateRaffleItemsUseCase } from "../../../useCases/raffle/UpdateOrCreateRaffleItemsUseCase";
-import { DrawRaffleUseCase } from "../../../useCases/raffle/DrawRaffleUseCase";
-import { TransactionStatusEnum } from "../../../enum/TransactionStatusEnum";
-import { Notify } from "../../../notifications";
-import { NewRaffleToApprove } from "../../../notifications/NewRaffleToApprove";
+import { Request, Response } from 'express'
+import { DateTime } from 'luxon'
+import { prisma } from '../../../prisma'
+import { RaffleStatusEnum } from '../../../enum/RaffleStatusEnum'
+import { InternalError } from '../../../config/GenerateErros'
+import { CreateRaffleRequest } from '../../../requests/CreateRaffleRequest'
+import { UpdateRaffleRequest } from '../../../requests/UpdateRaffleRequest'
+import { ValidateNumberOfMonthlyRafflesUseCase } from '../../../useCases/raffle/ValidateNumberOfMonthlyRafflesUseCase'
+import { UpdateOrCreateRaffleItemsUseCase } from '../../../useCases/raffle/UpdateOrCreateRaffleItemsUseCase'
+import { DrawRaffleUseCase } from '../../../useCases/raffle/DrawRaffleUseCase'
+import { TransactionStatusEnum } from '../../../enum/TransactionStatusEnum'
+import { Notify } from '../../../notifications'
+import { NewRaffleToApprove } from '../../../notifications/NewRaffleToApprove'
 
 const validateNumberOfMonthlyRaffles =
-  new ValidateNumberOfMonthlyRafflesUseCase();
+  new ValidateNumberOfMonthlyRafflesUseCase()
 
-const updateOrCreateRaffleItemsUseCase = new UpdateOrCreateRaffleItemsUseCase();
+const updateOrCreateRaffleItemsUseCase = new UpdateOrCreateRaffleItemsUseCase()
 
 export class RaffleController {
   async index(request: Request, response: Response) {
-    const { companyId } = request["tokenPayload"];
+    const { companyId } = request['tokenPayload']
 
     const raffles = await prisma.raffle.findMany({
       where: { companyId },
@@ -27,21 +27,21 @@ export class RaffleController {
         status: true,
         _count: { select: { items: true, tickets: true } },
       },
-      orderBy: { createdAt: "desc" },
-    });
+      orderBy: { createdAt: 'desc' },
+    })
 
-    return response.status(200).json(raffles);
+    return response.status(200).json(raffles)
   }
 
   async show(request: Request, response: Response) {
-    const { companyId } = request["tokenPayload"];
-    const { id } = request.params;
+    const { companyId } = request['tokenPayload']
+    const { id } = request.params
 
     const raffle = await prisma.raffle.findFirst({
       where: { id, companyId },
       include: {
         items: {
-          orderBy: { order: "asc" },
+          orderBy: { order: 'asc' },
           include: {
             raffleItemDelivery: {
               include: { companyUser: { select: { name: true } } },
@@ -57,16 +57,16 @@ export class RaffleController {
         },
         status: true,
       },
-    });
+    })
 
     if (!raffle) {
-      throw new InternalError("Sorteio não encontrado", 400);
+      throw new InternalError('Sorteio não encontrado', 400)
     }
 
     const consumers = await prisma.consumer.findMany({
       where: {
         raffleTickets: {
-          some: { raffleId: raffle.id, status: { not: "CANCELED" } },
+          some: { raffleId: raffle.id, status: { not: 'CANCELED' } },
         },
       },
       select: {
@@ -76,57 +76,57 @@ export class RaffleController {
         _count: {
           select: {
             raffleTickets: {
-              where: { raffleId: raffle.id, status: { not: "CANCELED" } },
+              where: { raffleId: raffle.id, status: { not: 'CANCELED' } },
             },
           },
         },
       },
-    });
+    })
 
-    return response.json({ ...raffle, consumers });
+    return response.json({ ...raffle, consumers })
   }
 
   async store(request: Request, response: Response) {
-    const { companyId } = request["tokenPayload"] as { companyId: string };
+    const { companyId } = request['tokenPayload'] as { companyId: string }
 
-    await validateNumberOfMonthlyRaffles.execute(companyId);
+    await validateNumberOfMonthlyRaffles.execute(companyId)
 
-    const form = CreateRaffleRequest.safeParse(request.body);
+    const form = CreateRaffleRequest.safeParse(request.body)
 
     if (!form.success) {
-      throw new InternalError("Existem erros nos dados enviados.", 422);
+      throw new InternalError('Existem erros nos dados enviados.', 422)
     }
 
     if (form.data.items.length === 0 || form.data.items.length > 20) {
       throw new InternalError(
-        "Deve ter pelo menos 1 prêmio e no máximo 20.",
-        400
-      );
+        'Deve ter pelo menos 1 prêmio e no máximo 20.',
+        400,
+      )
     }
 
     const isDrawDateInPast =
       new Date(form.data.drawDate) <
-      DateTime.now().minus({ hours: 3 }).startOf("day").toJSDate();
+      DateTime.now().minus({ hours: 3 }).startOf('day').toJSDate()
 
     if (isDrawDateInPast) {
       throw new InternalError(
-        "Não é possível criar sorteio com data passada.",
-        422
-      );
+        'Não é possível criar sorteio com data passada.',
+        422,
+      )
     }
 
     const waitingStatus = await prisma.raffleStatus.findFirst({
       where: { description: RaffleStatusEnum.WAITING },
-    });
+    })
 
     if (!waitingStatus) {
       throw new InternalError(
-        "Sistema não configurado. Procure um administrador",
-        400
-      );
+        'Sistema não configurado. Procure um administrador',
+        400,
+      )
     }
 
-    const { items, drawDate, ...rest } = form.data;
+    const { items, drawDate, ...rest } = form.data
 
     const raffle = await prisma.raffle.create({
       data: {
@@ -140,82 +140,82 @@ export class RaffleController {
         companyId,
         statusId: waitingStatus.id,
       },
-    });
+    })
 
-    await updateOrCreateRaffleItemsUseCase.execute(raffle.id, items);
+    await updateOrCreateRaffleItemsUseCase.execute(raffle.id, items)
 
     const users = await prisma.takebackUser.findMany({
       where: { userTypeId: 2, isActive: true },
-    });
+    })
 
     const company = await prisma.company.findFirst({
       where: { id: companyId },
-    });
+    })
 
     Notify.sendMany(
       users,
-      new NewRaffleToApprove(raffle.id, company.fantasyName)
-    );
+      new NewRaffleToApprove(raffle.id, company.fantasyName),
+    )
 
-    return response.status(201).json(raffle);
+    return response.status(201).json(raffle)
   }
 
   async update(request: Request, response: Response) {
-    const { companyId } = request["tokenPayload"];
-    const { id } = request.params;
+    const { companyId } = request['tokenPayload']
+    const { id } = request.params
 
     const raffle = await prisma.raffle.findFirst({
       where: { id, companyId },
       select: { id: true, statusId: true },
-    });
+    })
 
     if (!raffle) {
-      throw new InternalError("Sorteio não encontrado", 400);
+      throw new InternalError('Sorteio não encontrado', 400)
     }
 
-    const form = UpdateRaffleRequest.safeParse(request.body);
+    const form = UpdateRaffleRequest.safeParse(request.body)
 
     if (!form.success) {
-      throw new InternalError("Existem erros nos dados enviados.", 422);
+      throw new InternalError('Existem erros nos dados enviados.', 422)
     }
 
     if (form.data.items.length === 0 || form.data.items.length > 20) {
       throw new InternalError(
-        "Deve ter pelo menos 1 prêmio e no máximo 20.",
-        400
-      );
+        'Deve ter pelo menos 1 prêmio e no máximo 20.',
+        400,
+      )
     }
 
     const isDrawDateInPast =
       new Date(form.data.drawDate) <
-      DateTime.now().minus({ hours: 3 }).startOf("day").toJSDate();
+      DateTime.now().minus({ hours: 3 }).startOf('day').toJSDate()
 
     if (isDrawDateInPast) {
       throw new InternalError(
-        "Não é possível criar sorteio com data passada.",
-        422
-      );
+        'Não é possível criar sorteio com data passada.',
+        422,
+      )
     }
 
     const waitingStatus = await prisma.raffleStatus.findFirst({
       where: { description: RaffleStatusEnum.WAITING },
-    });
+    })
 
     if (!waitingStatus) {
       throw new InternalError(
-        "Sistema não configurado. Procure um administrador",
-        400
-      );
+        'Sistema não configurado. Procure um administrador',
+        400,
+      )
     }
 
     if (raffle.statusId !== waitingStatus.id) {
       throw new InternalError(
-        "Não é possível editar um sorteio já aprovado.",
-        400
-      );
+        'Não é possível editar um sorteio já aprovado.',
+        400,
+      )
     }
 
-    const { items, drawDate, ...rest } = form.data;
+    const { items, drawDate, ...rest } = form.data
 
     await prisma.raffle.update({
       where: { id: raffle.id },
@@ -223,16 +223,16 @@ export class RaffleController {
         ...rest,
         drawDate: DateTime.fromISO(drawDate).plus({ hours: 3 }).toISO(),
       },
-    });
+    })
 
-    await updateOrCreateRaffleItemsUseCase.execute(raffle.id, items);
+    await updateOrCreateRaffleItemsUseCase.execute(raffle.id, items)
 
-    return response.status(200).json({ message: "Atualizado com sucesso." });
+    return response.status(200).json({ message: 'Atualizado com sucesso.' })
   }
 
   async cancel(request: Request, response: Response) {
-    const { companyId } = request["tokenPayload"];
-    const { id } = request.params;
+    const { companyId } = request['tokenPayload']
+    const { id } = request.params
 
     const raffle = await prisma.raffle.findFirst({
       where: {
@@ -241,21 +241,21 @@ export class RaffleController {
         status: { description: RaffleStatusEnum.WAITING },
       },
       select: { id: true },
-    });
+    })
 
     if (!raffle) {
-      throw new InternalError("Sorteio não encontrado", 400);
+      throw new InternalError('Sorteio não encontrado', 400)
     }
 
     const cancelStatus = await prisma.raffleStatus.findFirst({
       where: { description: RaffleStatusEnum.CANCELED },
-    });
+    })
 
     if (!cancelStatus) {
       throw new InternalError(
-        "Sistema não configurado. Procure um administrador",
-        400
-      );
+        'Sistema não configurado. Procure um administrador',
+        400,
+      )
     }
 
     await prisma.raffle.update({
@@ -263,25 +263,25 @@ export class RaffleController {
       data: {
         statusId: cancelStatus.id,
       },
-    });
+    })
 
-    return response.json({ message: "Sorteio cancelado" });
+    return response.json({ message: 'Sorteio cancelado' })
   }
 
   async monthlyRemaining(request: Request, response: Response) {
-    const { companyId } = request["tokenPayload"];
+    const { companyId } = request['tokenPayload']
 
     const { numberMaxOfMonthlyRaffles, numberOfRaffleThisMonth } =
-      await validateNumberOfMonthlyRaffles.getCount(companyId);
+      await validateNumberOfMonthlyRaffles.getCount(companyId)
 
     return response.json({
       monthlyRemaining: numberMaxOfMonthlyRaffles - numberOfRaffleThisMonth,
-    });
+    })
   }
 
   async draw(request: Request, response: Response) {
-    const { companyId } = request["tokenPayload"];
-    const { id } = request.params;
+    const { companyId } = request['tokenPayload']
+    const { id } = request.params
 
     const pendingTransactions = await prisma.transaction.count({
       where: {
@@ -296,13 +296,13 @@ export class RaffleController {
           },
         },
       },
-    });
+    })
 
     if (pendingTransactions) {
       throw new InternalError(
-        "Não é possível realizar sorteios com cashbacks pendentes. Pague todos e tente sortear novamente.",
-        400
-      );
+        'Não é possível realizar sorteios com cashbacks pendentes. Pague todos e tente sortear novamente.',
+        400,
+      )
     }
 
     const raffle = await prisma.raffle.findFirst({
@@ -313,35 +313,35 @@ export class RaffleController {
         drawDate: { lte: new Date() },
       },
       include: {
-        items: { orderBy: { order: "asc" } },
+        items: { orderBy: { order: 'asc' }, where: { winnerTicketId: null } },
         company: { select: { fantasyName: true } },
       },
-    });
+    })
 
     if (!raffle) {
-      throw new InternalError("Não foi possível sortear ainda.", 400);
+      throw new InternalError('Não foi possível sortear ainda.', 400)
     }
 
     const deliveringStatus = await prisma.raffleStatus.findFirst({
       where: { description: RaffleStatusEnum.DELIVERING },
-    });
+    })
 
     if (!deliveringStatus) {
       throw new InternalError(
-        "Sistema não configurado. Procure um administrador",
-        400
-      );
+        'Sistema não configurado. Procure um administrador',
+        400,
+      )
     }
 
-    const useCase = new DrawRaffleUseCase();
+    const useCase = new DrawRaffleUseCase()
 
-    await useCase.execute(raffle);
+    await useCase.execute(raffle)
 
     await prisma.raffle.update({
       where: { id: raffle.id },
       data: { statusId: deliveringStatus.id },
-    });
+    })
 
-    return response.status(204).json();
+    return response.status(204).json()
   }
 }
