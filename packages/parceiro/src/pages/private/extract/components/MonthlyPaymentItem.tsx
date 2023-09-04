e7520fe8bb6e0cd352244328f93227147a166c27
Fabiano Santos <50252807+fabianosantos23@users.noreply.github.com>
2023-09-04 19:15:52 -0300

    feat(parceiro): atualiza componentes auxiliares

--- packages/parceiro/src/pages/private/extract/components/MonthlyPaymentItem.tsx
+++ packages/parceiro/src/pages/private/extract/components/MonthlyPaymentItem.tsx
@@ -10,20 +10,17 @@ type MonthlyPaymentItemProps = {
 }
 
 export function MonthlyPaymentItem(props: MonthlyPaymentItemProps) {
-  const { data, referenceDate, type } = props
+  const { data, referenceDate } = props
 
   return (
     <Tr color="gray.500" key={data.id}>
-      <Td px="2" fontSize="xs">
+      <Td px="2" fontSize="xs" w={200}>
         {new Date(referenceDate).toLocaleString()}
       </Td>
       <Td px="2" fontSize="xs">
-        <Text>{type}</Text>
+        <Text>Pagamento de Mensalidade</Text>
       </Td>
-      <Td px="2" fontSize="xs">
-        <Text>Mensalidade</Text>
-      </Td>
-      <Td px="2" fontSize="xs">
+      <Td px="2" fontSize="xs" w={150}>
         <Text fontWeight="semibold" color="red.600">
           -{currencyFormat(data.amountPaid)}
         </Text>
