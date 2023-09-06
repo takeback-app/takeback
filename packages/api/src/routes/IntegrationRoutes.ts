aa978c8a66a78e9fc04fc31d3d02b4b2eaf87d8a
Bruno <bfaa1999@gmail.com>
2023-09-06 09:14:57 -0300

    feat: adicionando rota teste para integração

--- packages/api/src/routes/IntegrationRoutes.ts
+++ packages/api/src/routes/IntegrationRoutes.ts
@@ -12,6 +12,7 @@ const routes = Router()
 
 routes.post('/login', LoginController.handle)
 routes.get('/updater/:target/:arch/:currentVersion', UpdaterController.handle)
+routes.get('/test', UpdaterController.test)
 
 routes.get('/bling/auth', HomologationController.handle)
 
