77cf93c5c6ccea5cc716725c52d4f6d496f2235d
Bruno <bfaa1999@gmail.com>
2023-09-18 08:47:32 -0300

    fix: bling

--- packages/api/src/controllers/integration/Bling/HomologationController.ts
+++ packages/api/src/controllers/integration/Bling/HomologationController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import { BlingIntegration } from '../../../useCases/Bling/BlingIntegration'
-import { BlingHomologation } from '../../../useCases/Bling/BlingHomologation'
+// import { BlingHomologation } from '../../../useCases/Bling/BlingHomologation'
 
 class HomologationController {
   async handle(request: Request, response: Response) {
@@ -13,17 +13,17 @@ class HomologationController {
 
     await integration.authenticate(code)
 
-    const blingHomologation = new BlingHomologation(integration)
+    // const blingHomologation = new BlingHomologation(integration)
 
-    const productData = await blingHomologation.getProduct()
-    const product = await blingHomologation.createProduct(productData)
-    await blingHomologation.updateProduct(product.id, {
-      nome: 'Copo',
-      preco: 32.56,
-      codigo: 'COD-4587',
-    })
-    await blingHomologation.setProductSituation(product.id)
-    await blingHomologation.deleteProduct(product.id)
+    // const productData = await blingHomologation.getProduct()
+    // const product = await blingHomologation.createProduct(productData)
+    // await blingHomologation.updateProduct(product.id, {
+    //   nome: 'Copo',
+    //   preco: 32.56,
+    //   codigo: 'COD-4587',
+    // })
+    // await blingHomologation.setProductSituation(product.id)
+    // await blingHomologation.deleteProduct(product.id)
 
     return response.status(204).json()
   }
