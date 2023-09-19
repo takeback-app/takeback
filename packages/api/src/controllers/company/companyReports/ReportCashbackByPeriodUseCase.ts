81fe4757fc981a14c044dc035ec03729386e5f81
Fabiano Santos <50252807+fabianosantos23@users.noreply.github.com>
2023-09-19 14:21:38 -0300

    feat(api): muda periodo de valores e dos gráfico

--- packages/api/src/controllers/company/companyReports/ReportCashbackByPeriodUseCase.ts
+++ packages/api/src/controllers/company/companyReports/ReportCashbackByPeriodUseCase.ts
@@ -1,76 +1,29 @@
-import { getRepository, In, IsNull } from "typeorm";
-import { Transactions } from "../../../database/models/Transaction";
-import { TransactionStatus } from "../../../database/models/TransactionStatus";
+import { BaseGraphUseCase } from './BaseGraphUseCase'
+import { prisma } from '../../../prisma'
+import { TransactionStatusEnum } from '../../../enum/TransactionStatusEnum'
 
-interface Props {
-  companyId: string;
-  userId: string;
-}
-
-class ReportCashbackByPeriodUseCase {
-  async execute({ companyId, userId }: Props) {
-    const date = new Date();
-    const today = date;
-    const sevenDaysAgo = new Date(date.setDate(date.getDate() - 7));
-
-    /* RELATÓRIO DE CASHBACK's DOS ÚLTIMOS 7 DIAS */
-
-    // Buscando os status de transações válidos
-    const transactionStatus = await getRepository(TransactionStatus).find({
-      select: ["id"],
+export class ReportCashbackByPeriodUseCase extends BaseGraphUseCase {
+  async getMonthlyValue(
+    monthStart: Date,
+    monthEnd: Date,
+    companyId: string,
+  ): Promise<number> {
+    const data = await prisma.transaction.aggregate({
       where: {
-        description: In(["Pendente", "Aprovada"]),
+        companiesId: companyId,
+        transactionStatus: {
+          description: {
+            in: [
+              TransactionStatusEnum.APPROVED,
+              TransactionStatusEnum.PAID_WITH_TAKEBACK,
+            ],
+          },
+        },
+        createdAt: { gte: monthStart, lt: monthEnd },
       },
-    });
+      _sum: { cashbackAmount: true },
+    })
 
-    // Criando array com os IDs dos tipos de transações válidas
-    const transactionStatusIds = [];
-    transactionStatus.map((item) => {
-      transactionStatusIds.push(item.id);
-    });
-
-    // Buscando as transações realizadas no período
-    const transactions = await getRepository(Transactions)
-      .createQueryBuilder("transactions")
-      .select("transactions.dateAt")
-      .addSelect("SUM(transactions.cashbackAmount)", "sum")
-      .where("transactions.companies = :companyId", { companyId })
-      .andWhere(
-        "transactions.dateAt > :sevenDaysAgo AND transactions.dateAt <= :today",
-        { sevenDaysAgo, today }
-      )
-      .andWhere("transactions.transactionStatus IN (:...transactionStatusId)", {
-        transactionStatusId: [...transactionStatusIds],
-      })
-      .groupBy("transactions.dateAt")
-      .orderBy("transactions.dateAt", "DESC")
-      .getRawMany();
-
-    // Formatando os dados para reposta
-    const days = [
-      "Domingo",
-      "Segunda",
-      "Terça",
-      "Quarta",
-      "Quinta",
-      "Sexta",
-      "Sábado",
-    ];
-    const daysToReturn = [];
-    const result = [];
-    let totalCashbacksValue = 0;
-    transactions.map((item) => {
-      result.push(item.sum);
-      daysToReturn.push(days[item.transactions_dateAt.getDay()]);
-
-      totalCashbacksValue = totalCashbacksValue + item.sum;
-    });
-
-    const values = result.reverse();
-    const labels = daysToReturn.reverse();
-
-    return { values, labels };
+    return +data._sum.cashbackAmount
   }
 }
-
-export { ReportCashbackByPeriodUseCase };
