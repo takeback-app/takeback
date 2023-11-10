6cee2e5658e394efeef9f9e971064fcfcb57402c
Fabiano Santos <50252807+fabianosantos23@users.noreply.github.com>
2023-11-10 11:41:58 -0300

    feat: muda latitude e longitude para float

--- packages/api/src/database/models/CompanyAddress.ts
+++ packages/api/src/database/models/CompanyAddress.ts
@@ -1,47 +1,49 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 
-import { City } from "./City";
+import { City } from './City'
 
 @Entity()
 export class CompaniesAddress {
-  @PrimaryGeneratedColumn("increment")
-  id: number;
+  @PrimaryGeneratedColumn('increment')
+  id: number
 
   @Column({
     nullable: true,
   })
-  street: string;
+  street: string
 
   @Column({
     nullable: true,
   })
-  district: string;
+  district: string
 
   @Column({
     nullable: true,
   })
-  number: string;
+  number: string
 
   @Column({
     nullable: true,
   })
-  complement: string;
+  complement: string
 
   @Column({
     nullable: true,
   })
-  zipCode: string;
+  zipCode: string
 
   @Column({
+    type: 'double precision',
     nullable: true,
   })
-  longitude: string;
+  longitude: number
 
   @Column({
+    type: 'double precision',
     nullable: true,
   })
-  latitude: string;
+  latitude: number
 
   @ManyToOne(() => City, (city) => city.companiesAdress)
-  city: City;
+  city: City
 }
